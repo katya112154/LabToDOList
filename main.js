d47240fd8e27d356975099d015d1468c05633da1
GrechkaEA <GrechkaEA@dom201.local>
2022-03-10 14:47:47 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById("add").addEventListener("click", () => {
+   console.log("Добавление элемента...");
+   const list = document.getElementsByClassName("todoList")[0];
+   const row = document.createElement("div");
+});
